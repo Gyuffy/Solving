d551a07c31758ba7878e11b6e0c5e6c40cc541b1
dan360 <dan360@naver.com>
2025-02-10 17:49:26 +0900

    민코딩 백트래킹의 탑 7번

---
+++ mincoding/Backtracking7/Backtracking7/Backtracking7.cpp
@@ -0,0 +1,63 @@
+﻿// 민코딩 Backtracking의 탑 7번 프로듀스 101배수
+#include <iostream>
+
+using namespace std;
+
+int N;
+int numbers[10];
+
+int ans = 0;
+int path[9];
+
+void input()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> numbers[i];
+	}
+}
+
+void calculate(int op)
+{
+	if(op == N - 1)
+	{
+		ans = path[0];
+		for (int i = 1; i < N; i++)
+		{
+			if (path[i-1] == 0)
+			{
+				ans += numbers[i];
+			}
+			else if (path[i - 1] == 1)
+			{
+				ans -= numbers[i];
+			}
+			else if (path[i - 1] == 2)
+			{
+				ans *= numbers[i];
+			}
+		}
+
+		if (ans % 101 == 0)
+		{
+			cout << ans << "\n";
+		}
+
+		return;
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		path[op] = i;
+		calculate(op + 1);
+	}
+}
+
+int main()
+{
+	input();
+	calculate(N - 1);
+
+	return 0;
+}
