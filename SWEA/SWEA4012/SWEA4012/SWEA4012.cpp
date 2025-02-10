2c6b6a1b1fd9a16940ae8fe26a40f05df4529b68
dan360 <dan360@naver.com>
2025-02-10 17:32:45 +0900

    요리사 문제 PASS

--- SWEA/SWEA4012/SWEA4012/SWEA4012.cpp
+++ SWEA/SWEA4012/SWEA4012/SWEA4012.cpp
@@ -1,15 +1,24 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
 int T, test_case, N;
+int taste_a, taste_b;
+
 int table[16][16];
 int choose_a[16];
 
+int diff, min_val;
+
 void init()
 {
+	min_val = 20001;
 
+	for (int i = 0; i < 16; i++)
+	{
+		choose_a[i] = 0;
+	}
 }
 
 void input()
@@ -26,17 +35,53 @@ void input()
 
 void cook(int row, int cnt)
 {
-	
-	for (int i = 0; i < N; i++)
+	// A가 N/2개의 요리 재료 선택을 끝내면 재귀 종료
+	if (cnt == N / 2)
 	{
-		if (row == i)
+		// 시너지 구하기 시작
+		taste_a = 0;
+		taste_b = 0;
+
+		for (int i = 0; i < N; i++)
 		{
-			continue;
+			for (int j = 0; j < N; j++)
+			{
+				// 두 가지의 재료가 동일하면 0이므로
+				if (i == j)
+				{
+					continue;
+				}
+
+				if (choose_a[i] && choose_a[j])
+				{
+					taste_a += table[i][j];
+				}
+				else if (!choose_a[i] && !choose_a[j])
+				{
+					taste_b += table[i][j];
+				}
+			}
 		}
-		choose_a[row] = i;
-		
-		
+
+		diff = (taste_a - taste_b) > 0 ? taste_a - taste_b : taste_b - taste_a;
+		if (diff < min_val)
+		{
+			min_val = diff;
+		}
+
+		return;
 	}
+
+	// row가 마지막까지 도달했으면 다시 재귀 종료
+	if (row >= N)
+	{
+		return;
+	}
+
+	choose_a[row] = 1;
+	cook(row + 1, cnt + 1);	// A가 선택한 경우
+	choose_a[row] = 0;
+	cook(row + 1, cnt);		// B가 선택한 경우
 }
 
 int main()
@@ -48,5 +93,8 @@ int main()
 		init();
 		input();
 		cook(0, 0);
+		cout << "#" << test_case << " " << min_val << "\n";
 	}
+
+	return 0;
 }
