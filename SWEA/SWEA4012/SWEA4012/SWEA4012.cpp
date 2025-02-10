bf5cc842d2b0ecfe4c7dd9e02209af2217f3c1b0
dan360 <dan360@naver.com>
2025-02-10 17:07:58 +0900

    SWEA 4012번 요리사

---
+++ SWEA/SWEA4012/SWEA4012/SWEA4012.cpp
@@ -0,0 +1,52 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int T, test_case, N;
+int table[16][16];
+int choose_a[16];
+
+void init()
+{
+
+}
+
+void input()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> table[i][j];
+		}
+	}
+}
+
+void cook(int row, int cnt)
+{
+	
+	for (int i = 0; i < N; i++)
+	{
+		if (row == i)
+		{
+			continue;
+		}
+		choose_a[row] = i;
+		
+		
+	}
+}
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	cin >> T;
+	for (test_case = 1; test_case <= T; test_case++)
+	{
+		init();
+		input();
+		cook(0, 0);
+	}
+}
