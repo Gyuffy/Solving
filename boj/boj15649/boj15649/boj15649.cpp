1fa67c56405b3e13ac1e43fc5240c4cee05c6597
dan360 <dan360@naver.com>
2025-02-11 13:45:39 +0900

    백준 15649번 PASS

---
+++ boj/boj15649/boj15649/boj15649.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, M;
+int path[8];
+int visited[8];
+
+void func(int lev)
+{
+	if (lev == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << path[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (visited[i-1] ==1)
+		{
+			continue;
+		}
+		path[lev] = i;
+		visited[i - 1] = 1;
+		func(lev + 1);
+		path[lev] = 0;
+		visited[i - 1] = 0;
+	}
+
+	
+
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	func(0);
+
+	return 0;
+}
