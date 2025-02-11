201951fd55784e7c2dda3def25af4143aad0e05a
dan360 <dan360@naver.com>
2025-02-11 14:06:50 +0900

    boj 15650 Fail

---
+++ boj/boj15650/boj15650/boj15650.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, M;
+int path[8];
+int visited[8];
+
+void func(int lev)
+{
+	if (lev == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << path[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (visited[i] == 1)
+		{
+			continue;
+		}
+		if (lev != 0 && path[lev - 1] > i)
+		{
+			continue;
+		}
+		visited[i] = 1;
+		path[lev] = i;
+		func(lev + 1);
+		path[lev] = -1;
+		visited[i] = 0;
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	func(0);
+
+	return 0;
+}
