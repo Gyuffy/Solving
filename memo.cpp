21d9eae674308e8f5d8d5965c8b2bd6da8f6ad0e
dan360 <dan360@naver.com>
2025-02-12 08:44:28 +0900

    memo commit

--- memo.cpp
+++ memo.cpp
@@ -2,11 +2,31 @@
 
 using namespace std;
 
-auto main() -> int
+struct memo
 {
+    /* data */
+    int a;
+};
+
+enum Juno
+{
+    UP,
+    LEFT
+};
+
+
+auto main(int argc, char* argv[]) -> int
+{
+    cout << argc << endl;
+
+    for (size_t i = 0; i < argc; i++)
+    {
+        cout << '[' << argv[i] << ']';
+    }
+    cout << endl;
     
     
-    
-    
+
+
     return 0;
 }
