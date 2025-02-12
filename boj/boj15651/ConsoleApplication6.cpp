797422eb0d451c12be05c688bc7550b4b8c1a9b5
dan360 <dan360@naver.com>
2025-02-12 12:09:04 +0900

    boj 15651번 PASS

---
+++ boj/boj15651/ConsoleApplication6.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, M;
+int path[8];
+
+void func(int lev)
+{
+	if (lev == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << path[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		path[lev] = i;
+		func(lev + 1);
+	}
+
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	func(0);
+
+
+	return 0;
+}
