71cb3bedfae79a3b3f10101fda68300f95f83ae6
dan360 <dan360@naver.com>
2025-02-14 00:44:53 +0900

    boj 26209 PASS

---
+++ boj/boj26209/boj26209/boj26209.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int bit[8];
+
+int main()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		cin >> bit[i];
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		if (bit[i] != 0 && bit[i] != 1)
+		{
+			cout << "F";
+			break;
+		}
+		else
+		{
+			if (i == 7)
+			{
+				cout << "S";
+			}
+		}
+	}
+
+	return 0;
+}
