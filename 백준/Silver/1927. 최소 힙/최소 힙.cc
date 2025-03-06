6eff351c2848e9c7fba1373b0ee2105a198696db
최규환 <dan360@naver.com>
2025-03-06 22:09:23 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct cmp
+{
+	bool operator()(int a, int b)
+	{
+		return a > b;
+	}
+};
+
+int N;
+priority_queue<int, vector<int>, cmp> pq;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	// freopen("sample_input.txt", "r", stdin);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		if (temp == 0 && pq.empty())
+		{
+			cout << 0 << "\n";
+		}
+		else if (temp == 0 && !pq.empty())
+		{
+			int ans = pq.top();
+			pq.pop();
+			cout << ans << "\n";
+		}
+		else
+		{
+			pq.push(temp);
+		}
+	}
+
+	return 0;
+}
