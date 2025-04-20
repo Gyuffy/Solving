b33f3a52e4a0b84044229b2e5cc0abadf8b1c7ef
최규환 <dan360@naver.com>
2025-04-20 15:39:10 +0900

    [D3] Title: 이진수 표현, Time: 8 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+ 
 int N, M;
-
+ 
 string solve()
 {
-	cin >> N >> M;
-	while (N--)
-	{
-		if (!(M & (1 << N)))
-			return "OFF";
-	}
-
-	return "ON";
+    cin >> N >> M;
+    while (N--)
+    {
+        if (!(M & (1 << N)))
+            return "OFF";
+    }
+ 
+    return "ON";
 }
-
+ 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int T, tc;
-	cin >> T;
-	for (tc = 1; tc <= T; tc++)
-	{
-		cout << "#" << tc << " " << solve() << "\n";
-	}
-
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int T, tc;
+    cin >> T;
+    for (tc = 1; tc <= T; tc++)
+    {
+        cout << "#" << tc << " " << solve() << "\n";
+    }
+ 
+    return 0;
 }
