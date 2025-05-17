7095ee1fba41247d4630e3d66ce1a3dc67fb6ee8
최규환 <dan360@naver.com>
2025-05-18 01:05:37 +0900

    [Silver III] Title: 화살표 그리기, Time: 32 ms, Memory: 2804 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15975. 화살표 그리기/화살표 그리기.cc
+++ 백준/Silver/15975. 화살표 그리기/화살표 그리기.cc
@@ -5,6 +5,15 @@ using namespace std;
 int N;
 pair<int, int> pt[100000];
 
+bool sorting(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	return a.second < b.second;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -16,22 +25,27 @@ int main()
 		cin >> pt[i].first >> pt[i].second;
 	}
 
+	sort(pt, pt + N, sorting);
+
 	long long len_sum = 0;
-	for (int i = 0; i < N; i++)
+	len_sum += pt[1].second == pt[0].second ? pt[1].first - pt[0].first : 0;
+	len_sum += pt[N - 1].second == pt[N - 2].second ? pt[N - 1].first - pt[N - 2].first : 0;
+	for (int i = 1; i < N - 1; i++)
 	{
-		long long min_val = 21e8;
-		for (int j = 0; j < N; j++)
+		if (pt[i].second != pt[i - 1].second && pt[i].second != pt[i + 1].second)
+			continue;
+		if (pt[i].second == pt[i - 1].second && pt[i].second == pt[i + 1].second)
 		{
-			if (i == j) continue;
-
-			if (pt[i].second == pt[j].second)
-			{
-				long long len = abs(pt[i].first - pt[j].first);
-				min_val = min(min_val, len);
-			}
+			len_sum += min(pt[i].first - pt[i - 1].first, pt[i + 1].first - pt[i].first);
 		}
-
-		len_sum += min_val != 21e8 ? min_val : 0;
+		else if (pt[i].second == pt[i - 1].second)
+		{
+			len_sum += pt[i].first - pt[i - 1].first;
+		}
+		else
+		{
+			len_sum += pt[i + 1].first - pt[i].first;
+		}
 	}
 
 	cout << len_sum << "\n";
