c3b707f215e88d6b9a6c8d823f216767c7200830
최규환 <dan360@naver.com>
2025-05-21 22:38:21 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+    int T; cin >> T;
+
+	int dp[41][2];
+
+	for (int tc = 0; tc < T; tc++)
+	{
+		int N;
+		cin >> N;
+
+		dp[0][0] = 1;
+		dp[0][1] = 0;
+		dp[1][0] = 0;
+		dp[1][1] = 1;
+		dp[2][0] = 1;
+		dp[2][1] = 1;
+
+		for (int i = 3; i <= N; i++)
+		{
+			dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+			dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+		}
+
+		cout << dp[N][0] << " " << dp[N][1] << "\n";
+	}
+	
+
+	return 0;
+}
