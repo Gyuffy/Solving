9d88e222c03ab9ab85254204dc85f27fe69ccc57
최규환 <dan360@naver.com>
2025-06-27 04:50:13 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    if (cin >> n)
+    {
+        cout << n * 4000 << '\n';
+    }
+    return 0;
+}
