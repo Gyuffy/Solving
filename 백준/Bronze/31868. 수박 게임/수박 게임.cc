bba4c273c3b4cb8de72e1121733d1c73b3c71958
최규환 <dan360@naver.com>
2025-07-21 00:50:44 +0900

    [Bronze III] Title: 수박 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+    for (int i = 1; i < N; ++i) {
+        K /= 2;
+    }
+    cout << K;
+    return 0;
+}
